97781dcd901f55c82c1ca2dd2b3c532d6b27403e
maks.kep.2000@mail.ru <maks.kep.2000@mail.ru>
2024-03-24 19:32:04 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,27 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private ApplicationDbContext _applicationDbContext;
+        public OrderService(ApplicationDbContext applicationDbContext)
+        {
+            this._applicationDbContext = applicationDbContext;
+        }
+
+        public Task<Order> GetOrder()
+        {
+            var orders = _applicationDbContext.Orders.OrderByDescending(order => order.Price * order.Quantity);
+            return Task.FromResult(orders.FirstOrDefault());
+        }
+      
+        public Task<List<Order>> GetOrders()
+        {
+            var orders = _applicationDbContext.Orders.Where(q => q.Quantity > 10).ToList();
+            return Task.FromResult(orders);
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+using System.Linq;
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private ApplicationDbContext _applicationDbContext;
+        private IOrderService _orderService;
+        public UserService(ApplicationDbContext applicationDbContext, IOrderService orderService)
+        {
+            this._applicationDbContext = applicationDbContext;
+            this._orderService = orderService;
+        }
+
+        public Task<User> GetUser()
+        {
+            var userId = _applicationDbContext.Orders.GroupBy(order => order.UserId).OrderByDescending(q => q.Count()).Select(group => group.Key).FirstOrDefault();
+            var user = _applicationDbContext.Users.FirstOrDefault(q => q.Id == userId);
+
+            return Task.FromResult(user); ;
+        }
+        public Task<List<User>> GetUsers()
+        {
+
+            var users = _applicationDbContext.Users.Where(q => q.Status == Enums.UserStatus.Inactive).ToList();
+
+            return Task.FromResult(users);
+        }
+    }
+}
